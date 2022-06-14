135a8a4f91a2bd859ed5ef0b982841c9cba823bb
kurshot <sonkurahmetkursat@gmail.com>
2022-06-14 19:14:49 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
-let viewButton  = document.querySelector('#btnViewUsers');
-let lboardBody = document.querySelector('.lboard__body');
-let userDivs = document.querySelectorAll('.lboard__user');
+let viewButton  = document.querySelector('#btnViewUsers'); // View Users butonu
+let lboardBody = document.querySelector('.lboard__body'); // Son 30 gün listesindeki kullanıcıları kapsayan div.
+let userDivs = lboardBody.querySelectorAll(':scope > .lboard__user'); // Son 30 gün listesindeki kullanıcılar arrayi.
+let last30Button = document.querySelector('#last30'); // Last 30 Days butonu
+let allTime = document.querySelector('#allTime'); // All-Time butonu
+let allTimeUsers = document.querySelector('.lboard__allTimeBody'); // All-time listesindeki kullancıları kapsayan div. 
+let count_allTimeUsers = allTimeUsers.querySelectorAll(':scope > .lboard__user').length; // All-time listesindeki kullanıcı sayısını tutar. 
 
 viewButton.addEventListener('click', function(){
     if(!viewButton.classList.contains('usersVisible')){
@@ -8,13 +12,31 @@ viewButton.addEventListener('click', function(){
         viewButton.classList.add('usersVisible');
         viewButton.innerHTML = "Hide Everyone";
         
-
     }
     else{
         viewButton.innerHTML = "View Everyone";
         viewButton.classList.remove('usersVisible');
         viewButton.classList.add('usersHidden');
+        allTimeUsers.style.display = "none";
         lboardBody.style.display = "none";
+        allTime.classList.remove('active');
+        last30Button.classList.add('active');
 
+    }
+})
+
+allTime.addEventListener('click', function(){
+    if(viewButton.classList.contains('usersVisible')){
+        allTimeUsers.style.display = "block";
+        last30Button.classList.remove('active');
+        allTime.classList.add('active');
+    }
+})
+
+last30Button.addEventListener('click', function(){
+    if(viewButton.classList.contains('usersVisible')){
+        allTimeUsers.style.display = "none";
+        allTime.classList.remove('active');
+        last30Button.classList.add('active');
     }
 })
