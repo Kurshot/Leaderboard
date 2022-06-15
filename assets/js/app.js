741c9c53eff7d8347ff3089273ba08ad68f46ed4
kurshot <sonkurahmetkursat@gmail.com>
2022-06-15 14:17:24 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,67 +1,23 @@
-let viewButton  = document.querySelector('#btnViewUsers'); // View Users butonu
-let lboardBody = document.querySelector('.lboard__body'); // Son 30 gün listesindeki kullanıcıları kapsayan div.
-let userDivs = lboardBody.querySelectorAll(':scope > .lboard__user'); // Son 30 gün listesindeki kullanıcılar arrayi.
-let last30Button = document.querySelector('#last30'); // Last 30 Days butonu
-let allTime = document.querySelector('#allTime'); // All-Time butonu
-let allTimeUsers = document.querySelector('.lboard__allTimeBody'); // All-time listesindeki kullancıları kapsayan div. 
-let count_allTimeUsers = allTimeUsers.querySelectorAll(':scope > .lboard__user').length; // All-time listesindeki kullanıcı sayısını tutar. 
+let last30Btn = document.querySelector('#last30');
+let allTimeBtn = document.querySelector('#allTime');
+let seeAllBtn  = document.querySelector('#seeAll');
 
-let click = 0;
+let last30Body = document.querySelector('.lboard__last30Body');
+let allTimeBody = document.querySelector('.lboard__allTimeBody');
+let seeAllBody = document.querySelector('.lboard__seeAllBody');
 
-viewButton.addEventListener('click', function(){
-    if(!viewButton.classList.contains('usersVisible')){
-        lboardBody.style.display = "block";
-        viewButton.classList.add('usersVisible');
-        viewButton.innerHTML = "Hide Everyone";
-        
-    }
-    else if(allTime.classList.contains('active') && viewButton.classList.contains('usersVisible')){
-        decreaseNumber();
-        viewButton.innerHTML = "View Everyone";
-        viewButton.classList.remove('usersVisible');
-        viewButton.classList.add('usersHidden');
-        allTimeUsers.style.display = "none";
-        lboardBody.style.display = "none";
-        allTime.classList.remove('active');
-        last30Button.classList.add('active');
-    }
-    else{
-        viewButton.innerHTML = "View Everyone";
-        viewButton.classList.remove('usersVisible');
-        viewButton.classList.add('usersHidden');
-        allTimeUsers.style.display = "none";
-        lboardBody.style.display = "none";
-        allTime.classList.remove('active');
-        last30Button.classList.add('active');
-    }
-})
-
-function increaseNumber(){
-    for(i=0; i < userDivs.length; i++){
-        userDivs[i].firstElementChild.firstElementChild.innerHTML = parseInt(userDivs[i].firstElementChild.firstElementChild.textContent, 10) + count_allTimeUsers +  `<img src="assets/images/pp${i + 1}.jpg">`; 
-    }
-    click++;
-}
-function decreaseNumber(){
-    for(i=0; i < userDivs.length; i++){
-        userDivs[i].firstElementChild.firstElementChild.innerHTML = parseInt(userDivs[i].firstElementChild.firstElementChild.textContent, 10) - count_allTimeUsers +  `<img src="assets/images/pp${i + 1}.jpg">`; 
-    }
+function btnClick(a, b, c){
+    b.style.display = "none";
+    c.style.display = "none";
+    a.style.display = "block";
 }
 
-allTime.addEventListener('click', function(){
-    if(viewButton.classList.contains('usersVisible') && !allTime.classList.contains('active')){
-        allTimeUsers.style.display = "block";
-        last30Button.classList.remove('active');
-        allTime.classList.add('active');
-        increaseNumber();
-    }
-})
-
-last30Button.addEventListener('click', function(){
-    if(viewButton.classList.contains('usersVisible') && !last30Button.classList.contains('active')){
-        allTimeUsers.style.display = "none";
-        allTime.classList.remove('active');
-        last30Button.classList.add('active');
-        decreaseNumber();
-    }
-})
+last30Btn.addEventListener('click', function(){
+    btnClick(last30Body, allTimeBody, seeAllBody);
+});
+allTimeBtn.addEventListener('click', function(){
+    btnClick(allTimeBody, last30Body, seeAllBody);
+});
+seeAllBtn.addEventListener('click', function(){
+    btnClick(seeAllBody, allTimeBody, last30Body);
+});
